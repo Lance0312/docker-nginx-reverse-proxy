cd9a596cee567d30051e399ee91ddd8d056a15a7
ShaoChen <shaochen@verdigris.co>
2018-07-25 13:25:19 +0800

    Update SSL cert renew method

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-certbot certonly --standalone -n --agree-tos -m it-tw@verdigris.co --domains app.verdigris.tw 
 # Add `--test-cert` back to use Let's Encrypt's staging server when trying to modify, test and play with this server
+certbot certonly --standalone -n --agree-tos -m it-tw@verdigris.co --domains app.verdigris.tw
 
-nginx -g "daemon off;"
+nginx -g "daemon off;" &
+
+# Not sure why but the cron job won't run inside docker instance on AWS EC2.
+# Instead of trying to run `cron`, just sleep and run `certbot renew`.
+while [ true ]; do
+    # Sleep for 12 hours
+    sleep 43200
+
+    # renew certs
+    certbot -q renew --renew-hook "/etc/init.d/nginx reload"
+done
